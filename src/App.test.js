34d2cca8bc18be7d7f5d49d2cb6012808138166f
luv-jeri <luvvjeri@gmail.com>
2022-11-28 16:20:28 +0530

    Complete Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,37 +1,58 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux';
-import appReducer from './reducer';
+import store from './store';
 
-const logger = ({ dispatch, getState }) => {
-  return (next) => {
-    return (action) => {
-      console.log('Action Type', action.type);
-      next(action);
-    };
-  };
-};
+afterEach(cleanup);
 
-const store = createStore(appReducer, applyMiddleware(logger));
+const spy = jest.spyOn(console, 'log');
 
 test('renders learn react link', () => {
   render(<App store={store} />);
 });
 
-describe('calls console.log', () => {
+describe('Should increase count', () => {
+  it('Should increase count when increase button is clicked', () => {
+    render(<App store={store} />);
+    
+    const increaseButton = screen.getByText('+');
+    expect(increaseButton).toBeInTheDocument();
+
+    const count = screen.getByText(/count/i);
+    expect(count).toBeInTheDocument();
+
+    fireEvent.click(increaseButton);
+    expect(count.textContent).toBe('Count: 1');
+  });
+});
+
+describe('Should log actions', () => {
   it('calls console.log when increase button is clicked', () => {
-    const spy = jest.spyOn(console, 'log');
     render(<App store={store} />);
     const increaseButton = screen.getByText('+');
-    increaseButton.click();
-    expect(spy).toHaveBeenCalledWith('Action Type', 'ADD_COUNT');
+
+    fireEvent.click(increaseButton);
+    expect(spy).toHaveBeenCalledWith('ADD_COUNT');
   });
 
   it('calls console.log when decrease button is clicked', () => {
-    const spy = jest.spyOn(console, 'log');
     render(<App store={store} />);
     const decreaseButton = screen.getByText('-');
-    decreaseButton.click();
-    expect(spy).toHaveBeenCalledWith('Action Type', 'SUBSTRACT_COUNT');
+
+    fireEvent.click(decreaseButton);
+    expect(spy).toHaveBeenCalledWith('SUBSTRACT_COUNT');
+  });
+});
+
+describe('Should not decrease count', () => {
+  it('Should not decrease count when decrease button is clicked', () => {
+    render(<App store={store} />);
+
+    const decreaseButton = screen.getByText('-');
+    const count = screen.getByText(/count/i);
+    count.textContent = 'Count: 0';
+
+    fireEvent.click(decreaseButton);
+    fireEvent.click(decreaseButton);
+    expect(count.textContent).toBe('Count: 0');
   });
 });
