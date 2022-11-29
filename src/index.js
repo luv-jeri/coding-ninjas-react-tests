30667b7a5f82da4a6d6be65b0010de681e94bc84
luv-jeri <luvvjeri@gmail.com>
2022-11-29 15:18:42 +0530

    -

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import store from './reducers/count';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Create a store and pass it to the Provider
 root.render(
   <React.StrictMode>
     <Provider store={store}>
