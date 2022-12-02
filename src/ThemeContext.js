996639695a7b14a2b67074d7ac6f0b5cc0662efa
luv-jeri <luvvjeri@gmail.com>
2022-12-02 13:12:06 +0530

    -

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,41 +1,9 @@
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext } from 'react';
 
 const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
-  const initialTheme = () => localStorage.getItem('CODING_NINJASA_PROJECT_THEME');
-
-  const [theme, setTheme] = useState(initialTheme);
-  
-  const toggleTheme = () =>
-    setTheme((theme) => {
-      if (theme === 'light-theme') return 'dark-theme';
-      if (theme === 'dark-theme') return 'light-theme';
-      return 'dark-theme';
-    });
-
-  useEffect(() => {
-    localStorage.setItem('CODING_NINJASA_PROJECT_THEME', theme);
-    if (theme === 'dark-theme') {
-      document.documentElement.classList.remove('light-theme');
-      document.documentElement.classList.add('dark-theme');
-    } else {
-      document.documentElement.classList.remove('dark-theme');
-      document.documentElement.classList.add('light-theme');
-    }
-  }, [theme]);
-
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+  return <ThemeContext.Provider value={{}}>{children}</ThemeContext.Provider>;
 };
-const useTheme = () => {
-  const context = useContext(ThemeContext);
-  if (context === undefined) {
-    throw new Error('useTheme must be used within a ThemeProvider');
-  }
-  return context;
-};
-export { ThemeProvider, useTheme };
+
+export { ThemeProvider };
