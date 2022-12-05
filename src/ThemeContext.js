be14edabb2b27c492733061363e3ea8a126b7234
luv-jeri <luvvjeri@gmail.com>
2022-12-05 17:44:17 +0530

    Removed Unused Code - ThemeContext

--- src/ThemeContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-const ThemeContext = createContext();
-
-const ThemeProvider = ({ children }) => {
-  return <ThemeContext.Provider value={{}}>{children}</ThemeContext.Provider>;
-};
-
-export { ThemeProvider };
