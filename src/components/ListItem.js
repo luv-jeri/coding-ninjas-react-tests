15c47945255a2ed45e404d1e048cf17649a72616
luv-jeri <luvvjeri@gmail.com>
2022-12-06 16:43:30 +0530

    Added - Class Solution

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,33 @@
+// import React from 'react';
+
+// // Use props to add styles, link and images so that it looks like the reference image.
+
+// const ListItem = (props) => {
+//   const { data } = props;
+//   return (
+//     <div className='listItem' style={{ backgroundColor: `${data.bgColor}` }}>
+//       <img src={data.icon} alt={data.name} />
+//       <a href={data.link}>{data.name}</a>
+//     </div>
+//   );
+// };
+
+// export default ListItem;
+
 import React from 'react';
 
 // Use props to add styles, link and images so that it looks like the reference image.
 
-const ListItem = (props) => {
-  const { data } = props;
-  return (
-    <div className='listItem' style={{ backgroundColor: `${data.bgColor}` }}>
-      <img src={data.icon} alt={data.name} />
-      <a href={data.link}>{data.name}</a>
-    </div>
-  );
-};
+class ListItem extends React.Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <div className='listItem' style={{ backgroundColor: `${data.bgColor}` }}>
+        <img src={data.icon} alt={data.name} />
+        <a href={data.link}>{data.name}</a>
+      </div>
+    );
+  }
+}
 
 export default ListItem;
