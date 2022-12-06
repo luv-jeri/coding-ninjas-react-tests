679bec6cec351b23754cfc99d4004e510b3a4938
luv-jeri <luvvjeri@gmail.com>
2022-12-06 12:48:01 +0530

    Added :  Solution

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,38 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 // Use React Hooks for incrementing, decrementing nd resetting the value of the counter.
 // Also, print the value of count on the console by using useEffect hook.
 
 function Counter() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
+  const handlePlus = () => {
+    setCount((state) => {
+      return state + 1;
+    });
+    // or setCount(count + 1);
+  };
+
+  const handleMinus = () => {
+    setCount((state) => {
+      return state - 1;
+    }); // or setCount(count - 1);
+  };
+
+  const handleReset = () => {
+    setCount((state) => {
+      return 0;
+    }); // or setCount(0);
+  };
   return (
     <>
-      <p>Count value is: </p>
-      <button>Reset</button>
-      <button>Plus (+)</button>
-      <button>Minus (-)</button>
+      <p>Count value is: {count}</p>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handlePlus}>Plus (+)</button>
+      <button onClick={handleMinus}>Minus (-)</button>
     </>
   );
 }
