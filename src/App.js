9871e965da1542f9962ec1a75ad26457a9d6e62a
luv-jeri <luvvjeri@gmail.com>
2022-12-07 12:12:48 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,74 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      number: 0,
-    };
-    this.handleAdd = this.handleAdd.bind(this);
-    this.handleSubract = this.handleSubract.bind(this);
-    this.handleSquare = this.handleSquare.bind(this);
-    this.handleDouble = this.handleDouble.bind(this);
-    this.handleDivide = this.handleDivide.bind(this);
-  }
-
-  handleAdd() {
-    this.setState({
-      number: this.state.number + 2,
-    });
-  }
-
-  handleDouble() {
-    this.setState({
-      number: this.state.number * 2,
-    });
-  }
-
-  handleSquare() {
-    this.setState({
-      number: this.state.number * this.state.number,
-    });
-  }
-
-  handleDivide() {
-    this.setState({
-      number: this.state.number / 2,
-    });
-  }
-
-  handleSubract = () => {
-    this.setState({
-      number: this.state.number - 2,
-    });
+const App = () => {
+  const [number, setNumber] = useState(0);
+  const handleAdd = () => {
+    setNumber(number + 2);
+  };
+  const handleSquare = () => {
+    setNumber(number * number);
+  };
+  const handleDouble = () => {
+    setNumber(number * 2);
+  };
+  const handleDivide = () => {
+    setNumber(number / 2);
+  };
+  const handleSubract = () => {
+    setNumber(number - 2);
   };
 
-  render() {
-    const { number } = this.state;
-    return (
-      <div className='container'>
-        <h1>{number}</h1>
-        <div className='buttons-wrapper'>
-          <button id='add' onClick={this.handleAdd}>
-            Add 2
-          </button>
-          <button id='square' onClick={this.handleSquare}>
-            Square{' '}
-          </button>
-          <button id='double' onClick={this.handleDouble}>
-            Double
-          </button>
-          <button id='divide' onClick={this.handleDivide}>
-            Divide by 2
-          </button>
-          <button id='subract' onClick={this.handleSubract}>
-            Subract 2
-          </button>
-        </div>
+  return (
+    <div className='container'>
+      <h1>{number}</h1>
+      <div className='buttons-wrapper'>
+        <button id='add' onClick={handleAdd}>
+          Add 2
+        </button>
+        <button id='square' onClick={handleSquare}>
+          Square{' '}
+        </button>
+        <button id='double' onClick={handleDouble}>
+          Double
+        </button>
+        <button id='divide' onClick={handleDivide}>
+          Divide by 2
+        </button>
+        <button id='subract' onClick={handleSubract}>
+          Subract 2
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
